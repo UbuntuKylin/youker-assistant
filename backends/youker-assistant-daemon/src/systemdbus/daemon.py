79f9d09087d119cc0963d0754e5f795e5675f701
kylinhebing@163.com <>
2013-09-11 13:52:57 +0800

    add the display information for clean by one key

--- backends/youker-assistant-daemon/src/systemdbus/daemon.py
+++ backends/youker-assistant-daemon/src/systemdbus/daemon.py
@@ -176,9 +176,17 @@ class Daemon(PolicyKitService):
     def clean_complete_main(self, msg):
         pass
 
+    @dbus.service.signal(INTERFACE, signature='s')
+    def clean_data_main(self, msg):
+        pass
+
     # a dbus method which means clean complete by second one key 
     @dbus.service.signal(INTERFACE, signature='s')
     def clean_complete_second(self, msg):
+        pass
+
+    @dbus.service.signal(INTERFACE, signature='s')
+    def clean_data_second(self, msg):
         pass
 
     # a dbus method which means clean complete
@@ -209,7 +217,6 @@ class Daemon(PolicyKitService):
         if not status:
             self.clean_complete_main_msg('')
             return
-        #tmp_mode_list = self.dbusstring_to_string(mode_list)
         cruft_dic = {}
         cruft_dic = self.daemononekey.get_scan_result(mode_list)
         if 'history' in cruft_dic:
@@ -221,6 +228,7 @@ class Daemon(PolicyKitService):
                 self.clean_error_main_msg('he')
             else:
                 self.clean_complete_main_msg('h')
+                sefl.clean_data_main_msg(cruft_dic['historydata'])
 
         if 'cookies' in cruft_dic:
             cookies_cruft_list = cruft_dic['cookies']
@@ -231,6 +239,7 @@ class Daemon(PolicyKitService):
                 self.clean_error_main_msg('ke')
             else:
                 self.clean_complete_main_msg('k')
+                sefl.clean_data_main_msg(cruft_dic['cookiesdata'])
 
         if 'cache' in cruft_dic:
             cache_cruft_list = cruft_dic['cache']
@@ -240,6 +249,7 @@ class Daemon(PolicyKitService):
                 self.clean_error_main_msg('ce')
             else:
                 self.clean_complete_main_msg('c')
+                sefl.clean_data_main_msg(cruft_dic['cachedata'])
         self.clean_complete_main_msg('o')
 
     # the function of clean cruft by second one key
@@ -261,6 +271,7 @@ class Daemon(PolicyKitService):
                 self.clean_error_second_msg('he')
             else:
                 self.clean_complete_second_msg('h')
+                sefl.clean_data_second_msg(cruft_dic['historydata'])
 
         if 'cookies' in cruft_dic:
             cookies_cruft_list = cruft_dic['cookies']
@@ -271,6 +282,7 @@ class Daemon(PolicyKitService):
                 self.clean_error_second_msg('ke')
             else:
                 self.clean_complete_second_msg('k')
+                sefl.clean_data_second_msg(cruft_dic['cookiesdata'])
 
         if 'cache' in cruft_dic:
             cache_cruft_list = cruft_dic['cache']
@@ -280,6 +292,7 @@ class Daemon(PolicyKitService):
                 self.clean_error_second_msg('ce')
             else:
                 self.clean_complete_second_msg('c')
+                sefl.clean_data_second_msg(cruft_dic['cachedata'])
         self.clean_complete_second_msg('o')
 
     @dbus.service.method(INTERFACE, in_signature='', out_signature='', sender_keyword='sender')
@@ -361,8 +374,14 @@ class Daemon(PolicyKitService):
     def clean_complete_main_msg(self, para):
         self.clean_complete_main(para)
 
+    def clean_data_main_msg(self, para):
+        self.clean_data_main(para)
+
     def clean_complete_second_msg(self, para):
         self.clean_complete_second(para)
+
+    def clean_data_second_msg(self, para):
+        self.clean_data_second(para)
 
     def clean_complete_msg(self, para):
         self.clean_complete(para)
