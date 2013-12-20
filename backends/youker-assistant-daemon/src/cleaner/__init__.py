d944d74a7405a6de8bfce98306177067b8574f8d
kylinhebing@163.com <>
2013-12-20 14:36:00 +0800

    change the type of data transmit by msg

--- backends/youker-assistant-daemon/src/cleaner/__init__.py
+++ backends/youker-assistant-daemon/src/cleaner/__init__.py
@@ -504,13 +504,22 @@ class CleanTheCache():
                 sesdaemon.data_transmit_by_cache('apt', one, 'False', '%s' % common.confirm_filesize_unit(os.path.getsize(one)))
 
         if 'software-center' in mode_list:
-            software_centerpath = "%s/.cache/software-center" % homedir
-            temp_software_center_list = self.objc.public_scan_cache(software_centerpath)
-            for one in temp_software_center_list:
+            softwarecenterpath = "%s/.cache/software-center" % homedir
+            temp_softwarecenter_list = self.objc.public_scan_cache(softwarecenterpath)
+            for one in temp_softwarecenter_list:
                 if os.path.isdir(one):
                     sesdaemon.data_transmit_by_cache('software-center', one, 'True', common.confirm_filesize_unit(common.get_dir_size(one)))
                 else:
                     sesdaemon.data_transmit_by_cache('software-center', one, 'False', common.confirm_filesize_unit(os.path.getsize(one)))
+
+        if 'thumbnails' in mode_list:
+            thumbnailspath = "%s/.cache/thumbnails" % homedir
+            temp_thumbnails_list = self.objc.public_scan_cache(thumbnailspath)
+            for one in temp_thumbnails_list:
+                if os.path.isdir(one):
+                    sesdaemon.data_transmit_by_cache('thumbnails', one, 'True', common.confirm_filesize_unit(common.get_dir_size(one)))
+                else:
+                    sesdaemon.data_transmit_by_cache('thumbnails', one, 'False',common.confirm_filesize_unit(os.path.getsize(one)))
         sesdaemon.cache_transmit_complete()
 
     def get_cache_crufts(self):
