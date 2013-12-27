9400677fc02a3f3e6483e0a39ccd316242b00f33
kylinhebing@163.com <>
2013-12-27 10:03:31 +0800

    add status msg for quick clean

--- backends/youker-assistant-daemon/src/systemdbus/daemon.py
+++ backends/youker-assistant-daemon/src/systemdbus/daemon.py
@@ -266,9 +266,9 @@ class Daemon(PolicyKitService):
     @dbus.service.signal(INTERFACE, signature='s')
     def clean_process_main(self, msg):
         pass
-
-    @dbus.service.signal(INTERFACE, signature='s')
-    def data_of_remove_file(self, msg):
+    
+    @dbus.service.signal(INTERFACE, signature='ss')
+    def status_for_quick_clean(self, flag, status):
         pass
 
     @dbus.service.signal(INTERFACE, signature='s')
@@ -557,13 +557,14 @@ class Daemon(PolicyKitService):
     # the function of clean files
     ### input-['filepath', 'file...]   output-''
     @dbus.service.method(INTERFACE, in_signature='ass', out_signature='', sender_keyword='sender')
-    def clean_file_cruft(self, cruftlist, flagstr, sender=None):
+    def clean_file_cruft(self, cruft_list, flagstr, sender=None):
         status = self._check_permission(sender, UK_ACTION_YOUKER)
         if not status:
             self.clean_complete_msg('')
             return
         try:
-            self.daemonclean.clean_the_file(cruftlist, self)
+            for cruft in cruft_list:
+                self.daemonclean.clean_the_file(cruft)
         except Exception, e:
             self.clean_error_msg(flagstr)
         else:
